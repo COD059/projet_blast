af138dfb6eb6450168f316ec21d41894f159a04f
COD059 <50121791+COD059@users.noreply.github.com>
2019-05-12 01:48:36 +0000

    Add files via upload

---
+++ blast.R
@@ -0,0 +1,10 @@
+library("rBLAST", lib.loc="~/R/win-library/3.5")
+download.file("ftp://ftp.ncbi.nlm.nih.gov/blast/db/16SMicrobial.tar.gz",
+              "16SMicrobial.tar.gz", mode='wb')
+untar("16SMicrobial.tar.gz", exdir="16SMicrobialDB")
+seq <- readAAStringSet(system.file("examples", "exampleAA.fasta", package="msa"))
+names(seq) <-  sapply(strsplit(names(seq), " "), "[", 1)
+
+bl <- blast(db="./16SMicrobialDB/16SMicrobial")
+
+cl <- predict(bl, seq[1,])
